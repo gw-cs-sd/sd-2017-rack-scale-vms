af45aa5f904830e625c59dbcca3085b5dbc71b4c
Neel Shah <neel@shah7.com>
2016-11-08 01:19:20 -0500

    Patches for libbdvmi to configure for AltP2m
      - Provides two patches which update libbdvmi for Altp2m Support
      - Init script has been updated to apply these patches ontop of setting
    up the directories

--- init.sh
+++ init.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-examples_dir="deps/libbdvmi/examples/"
+PWD=`pwd`
+RSA_DIR="RSA_App"
+LIBBDVMI_PATCH_DIR="patches/libbdvmi"
+LIBBDVMI_DIR="deps/libbdvmi"
+EXAMPLES_DIR="$LIBBDVMI_DIR/examples"
+
+libbdvmi_directory_setup()
+{
+    if [ ! -d "$EXAMPLES_DIR"/hookguest ]
+    then
+        mkdir "$EXAMPLES_DIR"/hookguest
+    else
+        rm -rf "$EXAMPLES_DIR"/hookguest/*
+    fi
+
+    mv "$EXAMPLES_DIR"/{hookguest.cpp,Makefile.am,Makefile.in} "$EXAMPLES_DIR"/hookguest/
+    cp -r RSA_App/* "$EXAMPLES_DIR"
+}
+
+apply_libbdvmi_patches()
+{
+    patch "$LIBBDVMI_DIR"/include/bdvmi/backendfactory.h  "$LIBBDVMI_PATCH_DIR"/backendfactory.h.patch
+    patch "$LIBBDVMI_DIR"/src/bdvmibackendfactory.cpp "$LIBBDVMI_PATCH_DIR"/bdvmibackendfactory.cpp.patch
+}
 
 git submodule update --init
-
-if [ ! -d "$examples_dir"/hookguest ]
-then
-    mkdir "$examples_dir"/hookguest
-else
-    rm -rf "$examples_dir"/hookguest/*
-fi
-
-mv "$examples_dir"/{hookguest.cpp,Makefile.am,Makefile.in} "$examples_dir"/hookguest/
-cp -r RSA_App/* "$examples_dir"
+libbdvmi_directory_setup
+apply_libbdvmi_patches
